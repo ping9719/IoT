fde39dc636da05bc2bd4fb72464409cd382b53cd
ping9719 <971931543@qq.com>
2025-04-16 11:07:53 +0800

    华普激光刻印

---
+++ Ping9719.IoT.WPF/Device/HuaPuMarkView.xaml.cs
@@ -0,0 +1,131 @@
+﻿using Ping9719.IoT.Device.Mark;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ping9719.IoT.WPF
+{
+    public partial class HuaPuMarkView : UserControl
+    {
+        public HuaPuMarkView()
+        {
+            InitializeComponent();
+        }
+
+        public HuaPuMark DeviceData
+        {
+            get { return (HuaPuMark)GetValue(DeviceDataProperty); }
+            set { SetValue(DeviceDataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceDataProperty =
+            DependencyProperty.Register("DeviceData", typeof(HuaPuMark), typeof(HuaPuMarkView), new PropertyMetadata(null));
+
+        private void jzmb(object sender, RoutedEventArgs e)
+        {
+            if (DeviceData == null)
+            {
+                textBoxInfo.AppendText("没有初始化设备\r\n");
+                return;
+            }
+
+            var bbb = DeviceData.Initialize(textBoxMbName.Text);
+            if (!bbb.IsSucceed)
+            {
+                textBoxInfo.AppendText($"{bbb.ErrorText}\r\n");
+                return;
+            }
+
+            textBoxInfo.AppendText($"成功加载模板");
+        }
+
+        private void ksth(object sender, RoutedEventArgs e)
+        {
+            if (DeviceData == null)
+            {
+                textBoxInfo.AppendText("没有初始化设备\r\n");
+                return;
+            }
+
+            var bbb = DeviceData.Data(textBoxKey.Text, textBoxName.Text);
+            if (!bbb.IsSucceed)
+            {
+                textBoxInfo.AppendText($"{bbb.ErrorText}\r\n");
+                return;
+            }
+
+            textBoxInfo.AppendText($"替换成功\r\n");
+        }
+
+        private void ksky(object sender, RoutedEventArgs e)
+        {
+            if (DeviceData == null)
+            {
+                textBoxInfo.AppendText("没有初始化设备\r\n");
+                return;
+            }
+
+            var bbb = DeviceData.MarkStart(checkBox1.IsChecked ?? false);
+            if (!bbb.IsSucceed)
+            {
+                textBoxInfo.AppendText($"{bbb.ErrorText}\r\n");
+                return;
+            }
+
+            textBoxInfo.AppendText($"打印完成，时间{bbb.TimeConsuming ?? 0}秒\r\n");
+        }
+
+        private void kshg(object sender, RoutedEventArgs e)
+        {
+            if (DeviceData == null)
+            {
+                textBoxInfo.AppendText("没有初始化设备\r\n");
+                return;
+            }
+
+            //var aaa = GetSelect();
+            //if (aaa.Length == 0)
+            //{
+            //    textBoxInfo.AppendText("请选择至少一个卡\r\n");
+            //    return;
+            //}
+            //var bbb = DeviceData.RedStart(aaa);
+            //if (!bbb.IsSucceed)
+            //{
+            //    textBoxInfo.AppendText($"{bbb.ErrorText}\r\n");
+            //    return;
+            //}
+
+            //textBoxInfo.AppendText($"红光完成，时间{bbb.Value}秒\r\n");
+        }
+
+        private void cxzt(object sender, RoutedEventArgs e)
+        {
+            if (DeviceData == null)
+            {
+                textBoxInfo.AppendText("没有初始化设备\r\n");
+                return;
+            }
+
+            var bbb = DeviceData.Stop();
+            if (!bbb.IsSucceed)
+            {
+                textBoxInfo.AppendText($"{bbb.ErrorText}\r\n");
+                return;
+            }
+
+            textBoxInfo.AppendText($"停止成功\r\n");
+        }
+    }
+}
---
+++ Ping9719.IoT/Device/Mark/HuaPuMark.cs
@@ -0,0 +1,169 @@
+﻿using Ping9719.IoT;
+using Ping9719.IoT.Algorithm;
+using Ping9719.IoT.Common;
+using Ping9719.IoT.Communication;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ping9719.IoT.Device.Mark
+{
+    /// <summary>
+    /// 华普激光刻印
+    /// </summary>
+    public class HuaPuMark
+    {
+        private static readonly Dictionary<byte, string> errCode = new Dictionary<byte, string>()
+        {
+            {1,"发现EZCAD在运行" },
+            {2,"找不到EZCAD.CFG" },
+            {3,"打开LMC1失败" },
+            {4,"没有有效的lmc1设备" },
+            {5,"lmc1版本错误" },
+            {6,"找不到设备配置文件" },
+            {7,"报警信号" },
+            {8,"用户停止" },
+            {9,"不明错误" },
+            {10,"超时" },
+            {11,"未初始化" },
+            {12,"读文件错误" },
+            {13,"窗口为空" },
+            {14,"找不到指定名称的字体" },
+            {15,"错误的笔号" },
+            {16,"指定名称的对象不是文本对象" },
+            {17,"保存文件失败" },
+            {18,"找不到指定对象" },
+            {19,"当前状态下不能执行此操作" },
+            {31,"重码" },
+            {32,"接受错误的消息" },
+        };
+        public ClientBase Client { get; private set; }
+        public HuaPuMark(ClientBase client, int timeout = 60000)
+        {
+            Client = client;
+            Client.ReceiveMode = ReceiveMode.ParseTime();
+            Client.Encoding = Encoding.ASCII;
+            Client.TimeOut = timeout;
+            Client.ConnectionMode = ConnectionMode.AutoOpen;
+        }
+
+        public HuaPuMark(string ip, int port = 1500, int timeout = 60000) : this(new TcpClient(ip, port)) { }
+
+        /// <summary>
+        /// 加载指定模板文件
+        /// </summary>
+        /// <param name="filePath">绝对路径</param>
+        /// <returns></returns>
+        public IoTResult Initialize(string filePath)
+        {
+            string comm = $"LOADFILE {filePath} \r\n";
+            try
+            {
+                var aaa = Client.SendReceive(comm);
+                if (!aaa.IsSucceed)
+                    return aaa;
+
+                return Analysis(aaa.Value);
+            }
+            catch (Exception ex)
+            {
+                return IoTResult.Create().AddError(ex);
+            }
+        }
+
+        /// <summary>
+        /// 替换打印模板
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="key">名称</param>
+        /// <param name="value">值</param>
+        /// <returns></returns>
+        public IoTResult Data(string key, string value)
+        {
+            string comm = $"SETVAR {key} {value} \r\n";
+            try
+            {
+                var aaa = Client.SendReceive(comm);
+                if (!aaa.IsSucceed)
+                    return aaa;
+
+                return Analysis(aaa.Value);
+            }
+            catch (Exception ex)
+            {
+                return IoTResult.Create().AddError(ex);
+            }
+        }
+
+        /// <summary>
+        /// 开始打印
+        /// </summary>
+        /// <param name="isAgain">是否强制打标，是不验证，否变量 0-9 是不会进行重码检验的，变量 10、11、12、13、14 会进行重码检验, 重码不打标</param>
+        /// <returns></returns>
+        public IoTResult MarkStart(bool isAgain = true)
+        {
+            string comm = isAgain ? "RUN AGAIN \r\n" : $"RUN \r\n";
+            try
+            {
+                var aaa = Client.SendReceive(comm);
+                if (!aaa.IsSucceed)
+                    return aaa;
+
+                return Analysis(aaa.Value);
+            }
+            catch (Exception ex)
+            {
+                return IoTResult.Create().AddError(ex);
+            }
+        }
+
+        /// <summary>
+        /// 停止指定设备刻印
+        /// </summary>
+        public IoTResult Stop()
+        {
+            string comm = $"STOP \r\n";
+            try
+            {
+                var aaa = Client.SendReceive(comm);
+                if (!aaa.IsSucceed)
+                    return aaa;
+
+                return Analysis(aaa.Value);
+            }
+            catch (Exception ex)
+            {
+                return IoTResult.Create().AddError(ex);
+            }
+        }
+
+        /// <summary>
+        /// 解析返回结果
+        /// </summary>
+        /// <param name="str">返回的结果</param>
+        /// <returns></returns>
+        private static IoTResult Analysis(string str)
+        {
+            if (!str.EndsWith("\r\n"))
+                return IoTResult.Create().AddError("返回结果格式错误，结尾不是换行");
+
+            if (byte.TryParse(str.Trim(), out byte b11))
+            {
+                if (b11 == 0)
+                    return IoTResult.Create();
+                else if (errCode.ContainsKey(b11))
+                    return IoTResult.Create().AddError(errCode[b11]);
+                else
+                    return IoTResult.Create().AddError($"返回结果格式错误，未知的错误代码[{b11}]");
+            }
+            else
+                return IoTResult.Create().AddError($"返回结果格式错误，不是有效的数字[{str.Trim()}]");
+        }
+    }
+}
