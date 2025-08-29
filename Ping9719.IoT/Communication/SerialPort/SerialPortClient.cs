2edc6e0f4163b7dff958650bb55f4aea35bb6782
ping9719 <971931543@qq.com>
2025-08-29 10:20:24 +0800

    connectString

--- Ping9719.IoT/Communication/SerialPort/SerialPortClient.cs
+++ Ping9719.IoT/Communication/SerialPort/SerialPortClient.cs
@@ -22,6 +22,78 @@ namespace Ping9719.IoT.Communication
         string portName; int baudRate; Parity parity = Parity.None; int dataBits = 8; StopBits stopBits = StopBits.One; Handshake handshake = Handshake.None;
         private System.IO.Ports.SerialPort serialPort;
 
+        /// <summary>
+        /// 初始化 串口客户端
+        /// </summary>
+        /// <param name="connectString">比如：COM7-19200-N-8-1。[串口号,COM开头]-[波特率,100以上]-[校验位,N,O,E,M,S]-[数据位,5-8]-[停止位,1,2,1.5/3]-[流控制,HN,HX,HR,HRX]</param>
+        public SerialPortClient(string connectString)
+        {
+            this.portName = "COM0";
+            this.baudRate = 9600;
+            this.dataBits = 8;
+            this.stopBits = StopBits.One;
+            this.parity = Parity.None;
+            this.handshake = Handshake.None;
+
+            ConnectionMode = ConnectionMode.Manual;
+            Encoding = Encoding.ASCII;
+            ReceiveMode = ReceiveMode.ParseTime();
+            ReceiveModeReceived = ReceiveMode.ParseTime();
+
+            if (string.IsNullOrWhiteSpace(connectString))
+                return;
+
+            foreach (string item in connectString.Split(new char[] { '-', '-', ',', '，', ';', '；' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (item.ToUpper().StartsWith("COM"))
+                    this.portName = item.ToUpper();
+                else if (item.ToUpper().StartsWith("H"))
+                {
+                    if (item.ToUpper().StartsWith("HN"))
+                        this.handshake = Handshake.None;
+                    else if (item.ToUpper().StartsWith("HX"))
+                        this.handshake = Handshake.XOnXOff;
+                    else if (item.ToUpper().StartsWith("HR"))
+                        this.handshake = Handshake.RequestToSend;
+                    else if (item.ToUpper().StartsWith("HRX"))
+                        this.handshake = Handshake.RequestToSendXOnXOff;
+                }
+                else if (item.ToUpper().StartsWith("N"))
+                    this.parity = Parity.None;
+                else if (item.ToUpper().StartsWith("O"))
+                    this.parity = Parity.Odd;
+                else if (item.ToUpper().StartsWith("E"))
+                    this.parity = Parity.Even;
+                else if (item.ToUpper().StartsWith("M"))
+                    this.parity = Parity.Mark;
+                else if (item.ToUpper().StartsWith("S"))
+                    this.parity = Parity.Space;
+                else if (double.TryParse(item.ToUpper(), out double douVal))
+                {
+                    if (douVal == 1)
+                        this.stopBits = StopBits.One;
+                    else if (douVal == 2)
+                        this.stopBits = StopBits.Two;
+                    else if (douVal == 1.5 || douVal == 3)
+                        this.stopBits = StopBits.OnePointFive;
+                    else if (douVal >= 5 && douVal <= 8)
+                        this.dataBits = (int)douVal;
+                    else if (douVal >= 100)
+                        this.baudRate = (int)douVal;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 初始化 串口客户端
+        /// </summary>
+        /// <param name="portName">串口号</param>
+        /// <param name="baudRate">波特率</param>
+        /// <param name="parity">校验位</param>
+        /// <param name="dataBits">数据位</param>
+        /// <param name="stopBits">停止位</param>
+        /// <param name="handshake">流控制</param>
         public SerialPortClient(string portName, int baudRate, Parity parity = Parity.None, int dataBits = 8, StopBits stopBits = StopBits.One, Handshake handshake = Handshake.None)
         {
             this.portName = portName;
--- Ping9719.IoT/Communication/TCP/TcpClient.cs
+++ Ping9719.IoT/Communication/TCP/TcpClient.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -19,12 +20,43 @@ namespace Ping9719.IoT.Communication
     {
         public override bool IsOpen => base.IsOpen && (tcpClient?.Connected ?? false);
         public Socket Socket => tcpClient?.Client;
-        string ip; int port;
+        string host; int port;
         private System.Net.Sockets.TcpClient tcpClient;
 
-        public TcpClient(string ip, int port)
+        /// <summary>
+        /// 初始化 串口客户端
+        /// </summary>
+        /// <param name="connectString">比如：127.0.0.1:502。</param>
+        public TcpClient(string connectString)
         {
-            this.ip = ip;
+            this.host = "127.0.0.1";
+            this.port = 502;
+
+            ConnectionMode = ConnectionMode.Manual;
+            Encoding = Encoding.ASCII;
+            ReceiveMode = ReceiveMode.ParseByteAll();
+            ReceiveModeReceived = ReceiveMode.ParseByteAll();
+
+            if (string.IsNullOrWhiteSpace(connectString))
+                return;
+
+            foreach (string item in connectString.Split(new char[] { ':', '：' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (int.TryParse(item.ToUpper(), out int intVal))
+                    this.port = intVal;
+                else
+                    this.host = item;
+            }
+        }
+
+        /// <summary>
+        /// 初始化 串口客户端
+        /// </summary>
+        /// <param name="host">主机地址，ip地址</param>
+        /// <param name="port">端口</param>
+        public TcpClient(string host, int port)
+        {
+            this.host = host;
             this.port = port;
 
             ConnectionMode = ConnectionMode.Manual;
@@ -65,7 +97,7 @@ namespace Ping9719.IoT.Communication
             tcpClient.ReceiveTimeout = TimeOut;
             tcpClient.SendTimeout = TimeOut;
 
-            var connectResult = tcpClient.BeginConnect(ip, port, null, null);
+            var connectResult = tcpClient.BeginConnect(host, port, null, null);
             if (!connectResult.AsyncWaitHandle.WaitOne(TimeOut))//阻塞当前线程
                 throw new TimeoutException("连接超时");
             tcpClient.EndConnect(connectResult);
